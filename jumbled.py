4950d20bd1e30f19bb9df58192f09819160163df
Fl0ris1 <149056628+Fl0ris1@users.noreply.github.com>
2025-06-29 10:43:51 +0200

    Add files via upload

--- jumbled.py
+++ jumbled.py
@@ -67,7 +67,30 @@ s_lbl=Label(root)
 def default():
     global main_words, answers, num
     text_lbl.config(text=main_words[num])
+
+def reset():
+    global main_words, answers, num
+    num=random.randrange(0,len(main_words),1)    
+    text_lbl.config(text=main_words[num])
+    input_box.delete(0,END)
+
+def check_ans():
+    global main_words, answers, num, score, q_count, score_txt, s_lbl
+    q_count+=1
+    input_answer=input_box.get()
+    if input_answer==answers[num]:
+        score+=1
+        messagebox.showinfo("CORRECT","YOUR ANSWER IS CORRECT")
     
+    else:
+        messagebox.showerror("INCORRECT","YOUR ANSWWER IS INCORRECT")
+
+    score_txt="score: "+str(score)+"/"+str(q_count)
+    s_lbl.forget()
+    s_lbl=Label(root,text=score_txt,font=("consolas",18,"bold"),bg="#8FB8DE",fg="#5438DC")
+    s_lbl.pack(side=LEFT)
+    reset()
+
 heading_lbl=Label(root,text="Jumbled Word Game",font=("consolas",18,"bold"),bg="#8FB8DE",fg="#EC5766")
 heading_lbl.pack(pady=5)
 
@@ -79,10 +102,10 @@ ans=StringVar()
 input_box=Entry(root,background="#8980F5",font=("consolas",16,"bold"),textvariable=ans)
 input_box.pack(ipadx=5,ipady=5)
 
-check_btn=Button(root,text="Check",font=("consolas",18,"bold"),bg="#5438DC",fg="#85BDBF",width=10)
+check_btn=Button(root,text="Check",font=("consolas",18,"bold"),bg="#5438DC",fg="#85BDBF",width=10,command=check_ans)
 check_btn.pack(pady=40)
 
-reset_btn=Button(root,text="Reset",font=("consolas",18,"bold"),bg="#5438DC",fg="#85BDBF",width=10)
+reset_btn=Button(root,text="Reset",font=("consolas",18,"bold"),bg="#5438DC",fg="#85BDBF",width=10,command=reset)
 reset_btn.pack()
 
 
@@ -91,6 +114,6 @@ reset_btn.pack()
 
 
 
-
+default()
 
 root.mainloop()
